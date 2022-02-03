b5328200066bdd331bedc214d878fb027237cd67
Mark Ortiz <ortizmark@hotmail.com>
2022-02-03 01:22:21 -0800

    Second Commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentAdminPortal.API.DataModels;
+using StudentAdminPortal.API.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentRepository _studentRepository;
+        private readonly IMapper _mapper;
+
+        public StudentController(IStudentRepository studentRepository, IMapper mapper)
+        {
+            _studentRepository = studentRepository;
+            _mapper = mapper;
+        }
+
+        ////https://localhost:44351/api/Student
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var students = await _studentRepository.GetStudents();
+            return Ok(_mapper.Map<List<Student>>(students));    
+            //return Ok(students);
+       }
+    }
+}
---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAdminPortal.API.DataModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private StudentAdminContext _context;
+        public StudentRepository(StudentAdminContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Student>> GetStudents()
+        {
+            return await _context.Student.Include(nameof(Gender)).Include(nameof(Address)).ToListAsync();  //Adds stuff in model but not in database
+            //return _context.Student.ToList();
+        }
+
+     }
+}
